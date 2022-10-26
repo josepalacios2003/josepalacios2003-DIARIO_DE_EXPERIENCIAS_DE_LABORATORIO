34ec141fba5273abb9ae527105cc572c318a96b3
josepalacios2003 <111389136+josepalacios2003@users.noreply.github.com>
2022-10-26 17:44:02 -0600

    Add files via upload

---
+++ Semana 11.0/L12+_+JAP1129522/L12+_+JAP1129522/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12___JAP1129522
+{
+    class PruebaVector2
+    {
+        private float[] notas;
+        private float promedio;
+
+        public void Cargar()
+        {
+            notas = new float[15];
+            for (int f = 0; f < 15; f++)
+            {
+                Console.Write("Ingrese la nota de curso:");
+                string linea = Console.ReadLine();
+                notas[f] = float.Parse(linea);
+            }
+        }
+        public void Totalnotas()
+        {
+            float total = 0;
+            for (int f = 0; f < 15; f++)
+            {
+                total = total + notas[f];
+            }
+            Console.WriteLine("El total de notas es:" + total);
+            Console.WriteLine();
+        }
+
+        public void CalcularPromedio()
+        {
+            float suma;
+            suma = 0;
+            for (int f = 0; f < 15; f++)
+            {
+                suma = suma + notas[f];
+            }
+            promedio = suma / 15;
+            Console.WriteLine("El promedio de notas es:" + promedio);
+        }
+
+        public void CALCULOMAYORES()
+        {
+            int mayor, menor;
+            mayor = 0;
+            menor = 0;
+            for (int f = 0; f < 15; f++)
+            {
+                if (notas[f] > promedio)
+                {
+                    mayor++;
+                }
+                else
+                {
+                    if (notas[f] < promedio)
+                    {
+                        menor++;
+                    }
+                }
+            }
+            Console.WriteLine("Cantidad de personas mayores al promedio:" + mayor);
+            Console.WriteLine("Cantidad de personas menores al promedio:" + menor);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector2 pv2 = new PruebaVector2();
+            pv2.Cargar();
+            pv2.Totalnotas();
+            pv2.CalcularPromedio();
+            pv2.CALCULOMAYORES();
+        }
+    }
+}
---
+++ Semana 11.0/L13+_+JAP1129522/L13+_+JAP1129522/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L13___JAP1129522
+{
+    class PruebaVector
+    {
+        private int[] edad;
+        private string[] nombres;
+
+        public void Cargar()
+        {
+            nombres = new string[5];
+            edad = new int[5];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("Ingrese el nombre: ");
+                nombres[f] = Console.ReadLine();
+                Console.Write("Ingrese la edad: ");
+                edad[f] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void MayoresEdad()
+        {
+            Console.WriteLine("Personas que son mayores de edad");
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                if (edad[f] >= 18)
+                {
+                    Console.WriteLine(nombres[f]);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            PruebaVector p = new PruebaVector();
+            p.Cargar();
+            p.MayoresEdad();
+            Console.ReadKey();
+        }
+    }
+}
