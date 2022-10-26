11d4a73923733f0c63bba45f094e91895f03a37a
josepalacios2003 <111389136+josepalacios2003@users.noreply.github.com>
2022-10-26 17:36:54 -0600

    Add files via upload

---
+++ Semana 7.0/L1+_+José Alejandro Carné1129522/L1+_+José Alejandro Carné1129522/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1___José_Palacios_carné1129522
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("soy José Alejandro");
+
+            /*comentarios*/
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("soy José Alejandro");
+            Console.ReadKey();
+
+            Console.WriteLine("José Alejandro");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("soy" + nombre);
+
+            Console.WriteLine("Hola Mundo");
+            Console.Write("soy" + nombre);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Semana 7.0/Tarea 1 José Palacios/Tarea 1 José Palacios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+
+            Console.WriteLine("Ingresa tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu edad: ");
+            string edad = Console.ReadLine();
+
+            Console.WriteLine("Igresa tu carrera: ");
+            string carrera = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu carne: ");
+            string carne = Console.ReadLine();
+
+            Console.WriteLine("Soy " + nombre + " tengo " + edad + " años y estudio la carrera de " + carrera + " y mi numero de carnet es: " + carne);
+
+            Console.ReadKey();
+
+        }
+    }
+}
