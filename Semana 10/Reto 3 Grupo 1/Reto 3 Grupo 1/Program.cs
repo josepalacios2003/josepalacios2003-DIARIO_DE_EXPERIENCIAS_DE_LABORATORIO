75b4a70babc2cb6d3c089756864ebcb1d36424ea
josepalacios2003 <111389136+josepalacios2003@users.noreply.github.com>
2022-11-10 18:56:11 -0600

    Add files via upload

---
+++ Semana 10/Reto 3 Grupo 1/Reto 3 Grupo 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    public static double calcularPrecio(string tipoHelado, int topping)
+    {
+        double precio = 0;
+        if (tipoHelado == "chocolate" || tipoHelado == "vainilla" || tipoHelado == "fresa")
+        {
+            precio = 5;
+        }
+        else if (tipoHelado == "napolitano" || tipoHelado == "pistacho")
+        {
+            precio = 7;
+        }
+        switch (topping)
+        {
+            case 1: // cobertura de chocolate
+                precio = precio + 1;
+                break;
+            case 2: // cobertura de chocolate con mania
+                precio = precio + 2;
+                break;
+            case 3: // cobertura de chocolate con anicillos
+                precio = precio + 1;
+                break;
+        }
+        return precio;
+    }
+    static void Main()
+    {
+
+    }
+}
